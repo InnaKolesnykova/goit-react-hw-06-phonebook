9f06257efbdf64bc5690be90e26cfc23729a0379
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-14 12:55:35 +0300

    с
    с

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,25 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
-import { contactsReducer } from './contactSlice';
-import { filterReducer } from './filterSlice'; 
+import storage from 'redux-persist/lib/storage';
+
+import { contactsReducer } from './contactsSlice';
+import filterReducer from './filterSlice';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['contacts'],
+  whitelist: ['contacts'], // зберігаємо тільки контакти
 };
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer, 
-});
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware({
-    serializableCheck: false,
-  }),
 });
 
 export const persistor = persistStore(store);
