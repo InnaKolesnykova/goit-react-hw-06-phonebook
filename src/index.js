6b06a7c66db5df94d550ca4a52073e20c29d5ac9
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-16 12:45:16 +0300

    а
    а

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filterSlice';
 
 const persistConfig = {
   key: 'contacts',
   storage,
-  whitelist: [], // якщо хочемо зберігати все (у нашому випадку це масив контактів)
 };
 
-const persistedContactsReducer = persistReducer(
-  { ...persistConfig, whitelist: [] },
-  contactsReducer
-);
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: persistedContactsReducer,
-    filter: filterReducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: persistedReducer,
 });
 
 export const persistor = persistStore(store);
